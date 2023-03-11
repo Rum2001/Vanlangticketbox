efd4222ac956c603739f801da97839d7dd69dcd2
Rum2001 <61676156+Rum2001@users.noreply.github.com>
2023-03-11 10:45:48 +0700

    add mit

---
+++ vanlangticketbox-app/database/migrations/2023_03_09_024709_create_questions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->unsignedBigInteger('attendee_id');
+            $table->foreign('attendee_id')->references('id')->on('attendees');
+            $table->text('content');
+            $table->text('answer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
